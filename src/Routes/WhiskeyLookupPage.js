ea255284c2bb082f6957c44893de47790acd5fa0
Robin Khiv <robinkhiv@gmail.com>
2019-05-08 09:30:19 -0700

    adding loading page to main screen

--- src/Routes/WhiskeyLookupPage.js
+++ src/Routes/WhiskeyLookupPage.js
@@ -3,11 +3,12 @@ import WhiskeySearchContext from '../Context/WhiskeySearchContext';
 import WhiskeyApiService from '../Services/whiskey-api-service';
 import WhiskeyListItem from '../Components/WhiskeyListItem/WhiskeyListItem';
 import WhiskeySearchForm from '../Components/WhiskeySearchForm/WhiskeySearchForm';
+import loadingpic from '../../src/loading picture/loading.gif';
 
 export default class WhiskeyLookupPage extends Component {
   static contextType = WhiskeySearchContext
-
-  componentDidMount() {
+ 
+  componentDidMount() {   
     this.context.clearError()
     WhiskeyApiService.getWhiskeys()
       .then(this.context.setWhiskeyList)
@@ -20,14 +21,21 @@ export default class WhiskeyLookupPage extends Component {
       <WhiskeyListItem key={whiskey.id} whiskey={whiskey}/>
       )
   }
+  renderLoading(){
+    return (
+      <img className='col-3' src={loadingpic} alt='dripping alcohol'></img>
+    )
+  }
 
   render() {
+    const {whiskeyFilter } =this.context; 
     return (
     <React.Fragment>
       <header className="whiskey-search-container row">
         <WhiskeySearchForm/>
       </header>
       <section className="whiskeyListPage row">
+        {!whiskeyFilter.length && this.renderLoading()}
         {this.renderWhiskeys()}
       </section>
     </React.Fragment>
