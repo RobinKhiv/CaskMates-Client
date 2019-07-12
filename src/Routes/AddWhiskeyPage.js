def2d06f05feb4d8e67f13fc6dba8eab77626327
Robin Khiv <robinkhiv@gmail.com>
2019-07-11 21:28:42 -0700

    changed addWhiskeyPage from class to function

--- src/Routes/AddWhiskeyPage.js
+++ src/Routes/AddWhiskeyPage.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import WhiskeyForm from '../Components/WhiskeyForm/WhiskeyForm'
 
-export default class AddWhiskeyPage extends Component {
-  handleAddWhiskeySuccess = (whiskeyId) => {
-    this.props.history.push(`/whiskeys/${whiskeyId}`)
-  }
-  render() {
-    return (
-      <section className='new-whiskey-form container row'>
-        <WhiskeyForm onAddSuccess={this.handleAddWhiskeySuccess}/>
-      </section>
-    )
-  }
+const addWhiskeyPage = props => {
+  const handleAddWhiskeySuccess = whiskeyId => {
+    props.history.push(`/whiskeys/${whiskeyId}`)
+  };
+  return (
+    <section className='new-whiskey-form container row'>
+      <WhiskeyForm onAddSuccess={handleAddWhiskeySuccess}/>
+    </section>
+  ) 
 }
+
+export default addWhiskeyPage;
