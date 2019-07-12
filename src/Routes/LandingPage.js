c794355ebf97c523cc8255f42fb239154de73786
Robin Khiv <robinkhiv@gmail.com>
2019-07-11 21:24:41 -0700

    changed landing page to function instead of class

--- src/Routes/LandingPage.js
+++ src/Routes/LandingPage.js
@@ -1,37 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import RegistrationForm from '../Components/RegistrationForm/Registration';
 
 import './LandingPage.css';
 
-export default class LandingPage extends Component {
-  static defaultProps ={
-    history: {
-        push: () => {}
-    }
+const landingPage = props => {
+  const handleRegistrationSuccuess = () => {
+    props.history.push('/login')
   }
 
-  handleRegistrationSuccuess = user => {
-    this.props.history.push('/login');
-  }
-
-  render() {
-    return (
-      <section className='landing-registration-page row'>
-      
-        <div className='landing-page-container col-4'>
-          <h3 className='Logo'>Caskmates</h3>
-          <p> This is your one stop for all the info you'll ever need on whiskey needs. You'll be able to construct your own 
-            personalized list with your favorite drink, keep track of new whiskeys you can not wait to try or
+  return (
+    <section className='landing-registration-page row'>
+      <div className='landing-page-container col-4'>
+        <h3 className='Logo'>Caskmates</h3>
+        <p> This is your one stop for all the info you'll ever need on whiskey needs. You'll be able to construct your own 
+          personalized list with your favorite drink, keep track of new whiskeys you can not wait to try or
           simply share your expertise so others can see what they are missing out!</p>
-        </div>
-        <div className="registration-container col-4">
-          <RegistrationForm 
-            onRegistrationSuccess={
-              this.handleRegistrationSuccuess
-            }
-          />
       </div>
-      </section>
-    )
-  }
-}
+      <div className="registration-container col-4">
+        <RegistrationForm 
+          onRegistrationSuccess={
+            () => handleRegistrationSuccuess()
+          }
+        />
+    </div>
+    </section>
+  )
+ }
+
+ export default landingPage;
