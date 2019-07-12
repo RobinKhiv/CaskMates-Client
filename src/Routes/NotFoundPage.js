fd63574aa6ef676d01411403fd50c91a7c17461f
Robin Khiv <robinkhiv@gmail.com>
2019-07-11 21:14:57 -0700

    changed notFoundPage from class to function

--- src/Routes/NotFoundPage.js
+++ src/Routes/NotFoundPage.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-export default class NotFoundPage extends Component {
-  render() {
-    return (
-      <section className='NotFoundPage'>
-        <h2>404 - Page not found</h2>
-        <p>Try going back to your previous page.</p>
-      </section>
-    )
-  }
-}
+const notFoundPage = () => {
+  return (
+    <section className='NotFoundPage'>
+      <h2>404 - Page not found</h2>
+      <p>Try going back to your previous page.</p>
+    </section>)
+};
+export default notFoundPage;
