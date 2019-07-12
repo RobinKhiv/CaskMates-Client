2b0b8a34e76753483b1325d549e553932e018331
Robin Khiv <robinkhiv@gmail.com>
2019-07-11 23:46:13 -0700

    added react hooks to listpage and changed to dynamic function

--- src/Routes/ListPage.js
+++ src/Routes/ListPage.js
@@ -1,54 +1,50 @@
-import React, { Component } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import WhiskeyApiService from '../Services/whiskey-api-service';
 import WhiskeyListContext from '../Context/WhiskeyListContext';
 import UserListItem from '../Components/UserListItem/UserListItem';
 import './ListPage.css'
 
-export default class ListPage extends Component {
-  static contextType = WhiskeyListContext;
-  state = {
-    currentView: ''
-  }
+const listPage = () => {
+  const [currentView, setView] = useState('');
+  const whiskeyList = useContext(WhiskeyListContext);
+  const {favoriteList, wishList, alreadyTried} = whiskeyList;
 
-  componentDidMount(){
+  useEffect(() => {
     WhiskeyApiService.getWhiskeyList()
-    .then(whiskeyList => 
-      this.context.setWhiskeyList(whiskeyList))
-    .catch(this.context.setError)
-  }
+    .then(whiskeys => 
+      whiskeyList.setWhiskeyList(whiskeys))
+    .catch(whiskeyList.setError)
+  }, [])
 
-  handleListRender = list => {
-    this.setState({currentView: list})
+  const handleListRender = list => {
+    setView(list);
   }
   
-  renderList(list){
+  const renderList = list => {
     return list.map(whiskey=> <UserListItem key={whiskey.id} whiskey={whiskey}/>)
   }
-  
-  render() {
-    const { alreadyTried, favoriteList, wishList } = this.context;
-    const { currentView } = this.state;
-    return (
-      <React.Fragment>
-        <header className="row">
-          <form className="list-selection-form col-12"onChange={this.handleListChange}>
-            <label htmlFor="list-selection">Select Your List</label>
-            <div className="list-selection-button-container">
-              <button type="button" name="list-selection" onClick={() => this.handleListRender('Favorite List')}>Favorite List</button>
-              <button type="button" name="list-selection" onClick={() => this.handleListRender('Wish List')}>Wish List</button>
-              <button type="button" name="list-selection" onClick={() => this.handleListRender('Already Tried List')}>Already Tried</button>
-            </div>
-          </form>
-        </header>
-        <section className='row'>
-          <div className="userListTitle col-12">
-           <h2>{currentView}</h2>
+
+  return (
+    <React.Fragment>
+      <header className="row">
+        <form className="list-selection-form col-12">
+          <label htmlFor="list-selection">Select Your List</label>
+          <div className="list-selection-button-container">
+            <button type="button" name="list-selection" onClick={() => handleListRender('Favorite List')}>Favorite List</button>
+            <button type="button" name="list-selection" onClick={() => handleListRender('Wish List')}>Wish List</button>
+            <button type="button" name="list-selection" onClick={() => handleListRender('Already Tried List')}>Already Tried</button>
           </div>
-          {currentView === 'Favorite List' && this.renderList(favoriteList)}
-          {currentView === 'Wish List' && this.renderList(wishList)}
-          {currentView === 'Already Tried List' && this.renderList(alreadyTried)}
-        </section>
-      </React.Fragment>
-    )
-  }
+        </form>
+      </header>
+      <section className='row'>
+        <div className="userListTitle col-12">
+         <h2>{currentView}</h2>
+        </div>
+        {currentView === 'Favorite List' && renderList(favoriteList)}
+        {currentView === 'Wish List' && renderList(wishList)}
+        {currentView === 'Already Tried List' && renderList(alreadyTried)}
+      </section>
+    </React.Fragment>
+  )
 }
+export default listPage;
